3c17a37e1430a641a6ffe9e4d626c204a871aaa9
Elton Garbin <eltongarbin@gmail.com>
2019-03-04 22:09:46 -0300

    add a list of posts to a gatsby blog with a graphql page query

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,21 +26,17 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
+          <main style={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+          }}>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.frontmatter.path}>{node.frontmatter.title}</div>
+      ))}
+    </div>
   </Layout>
 )
 
