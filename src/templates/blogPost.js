c2cc2895cf86e59bb5fa74fe736a4deaeec4fc46
Elton Garbin <eltongarbin@gmail.com>
2019-03-18 22:20:13 -0300

    add next and previous links

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 
-const Template = () => <div>Blog Post Here</div>;
+const Template = ({
+  data: { markdownRemark },
+  pageContext: { next, prev }
+}) => (
+  <div>
+    <h1>{markdownRemark.frontmatter.title}</h1>
+    <div
+      className="blogpost"
+      dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
+    />
+    {next && (
+      <Link style={{ display: 'block' }} to={next.frontmatter.path}>
+        Next
+      </Link>
+    )}
+    {prev && (
+      <Link style={{ display: 'block' }} to={prev.frontmatter.path}>
+        Prev
+      </Link>
+    )}
+  </div>
+);
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
 
 export default Template;
